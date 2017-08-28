64e887089460d18492a80112236336968b66d06e
KADC\amee.prajapati <amee.engg@gmail.com>
2017-08-28 16:44:29 +0530

    no message

--- BackendAPI/main.js
+++ BackendAPI/main.js
@@ -243,11 +243,11 @@ var registerNotification = function (sendUser, receiveUser, type, id) {
 				console.log('step4 ++++ badge number');
 				
 				if (user.hasOwnProperty("deviceToken")) {
-					var notificationCount;
+					var notificationCount = 0;
 					notificationRef.orderByChild("sendUser").equalTo(receiveUser).once("value", function (snapshot) {
                         if (!snapshot.exists()) return;
                         console.log("snapshot.val().isAlreadyViewed" + snapshot.val().isAlreadyViewed);
-                        console.log("snapshot.val()" + snapshot.val());
+                        console.log("snapshot.val()" , snapshot.val());
 
                         if(snapshot.val() && snapshot.val().isAlreadyViewed == false){                        	
 	                        notificationCount = snapshot.numChildren();
