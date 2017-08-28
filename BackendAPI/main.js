c38e24e1338c2d0d1b92139ade41289ce8145b6e
KADC\amee.prajapati <amee.engg@gmail.com>
2017-08-28 16:38:32 +0530

    - revert code

--- BackendAPI/main.js
+++ BackendAPI/main.js
@@ -243,12 +243,18 @@ var registerNotification = function (sendUser, receiveUser, type, id) {
 				console.log('step4 ++++ badge number');
 				
 				if (user.hasOwnProperty("deviceToken")) {
-					notificationRef.orderByChild("isAlreadyViewed").equalTo(false).once("value", function (snapshot) {
-						//console.log("ameee "+ snapshot.exists());
+					notificationRef.orderByChild("sendUser").equalTo(receiveUser).once("value", function (snapshot) {
                         if (!snapshot.exists()) return;
-                        var notificationCount = snapshot.numChildren();                        
-                        console.log("ameee 11111");
-                        console.log("notificationCount ====" + notificationCount);
+                        console.log("snapshot.val().isAlreadyViewed" + snapshot.val().isAlreadyViewed);
+                        console.log("snapshot.val()" + snapshot.val());
+
+                        if(snapshot.val() && snapshot.val().isAlreadyViewed == false){                        	
+	                        var notificationCount = snapshot.numChildren();
+	                        console.log("ameee 11111");
+	                        console.log("notificationCount ====" + notificationCount);
+                        }
+                    });
+                        
 					//add sound in notification: i.e notification.body, sound
 					var message = {
 						to: user.deviceToken, // required fill with device token or topics
@@ -262,8 +268,7 @@ var registerNotification = function (sendUser, receiveUser, type, id) {
                             badge: notificationCount						
 						}
 					};
-					sendPushNotification(message)
-                    });
+					sendPushNotification(message)                    
 				}
 			});
 
